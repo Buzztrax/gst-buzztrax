db8f43a409c93b9b27d71d573b6e4d7fb1bb36cb
Stefan Sauer <ensonic@users.sf.net>
2009-05-10 19:51:06 +0000

    * autogen.sh: 	  aclocal gather all the necessary macros. And libtoolize installs m4 files. 	  They must be present before running aclocal. Without this patch, autoconf 	  complains about unknown libtool macros (using 2.2.6a-4 on Debian).

--- autogen.sh
+++ autogen.sh
@@ -351,11 +351,13 @@ fi
 
 toplevel_check $srcfile
 
+# must be run before aclocal, as this installs some m4 files
+tool_run "$libtoolize" "--copy --force"
+
 # aclocal
 if test -f acinclude.m4; then rm acinclude.m4; fi
 tool_run "$aclocal" "-I m4 $ACLOCAL_FLAGS"
 
-tool_run "$libtoolize" "--copy --force"
 if test -n "$gtkdocize"; then
   if test "x$have_gtkdoc_1_9" = "x0"; then
     tool_run "$gtkdocize" "--copy"
